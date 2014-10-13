9dbbc9765bfbfe33d709753cb487e88ae356eae9
Marko <mzubanovic@gmail.com>
2014-10-13 20:18:03 +0200

    Adding Facebook API

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 <footer>
 	<p><span><?php echo date('Y') ?> &copy; Bel Bosco</span></p>
+	<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fbelbosco.net%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80" scrolling="no" frameborder="2" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
+
 </footer>
